c986d8dbf9a5769392faa5cf88cf3374c2adb659
Alimey <71897307+Alimey@users.noreply.github.com>
2022-11-29 15:33:51 +0300

    Bullet

---
+++ bullet.py
@@ -0,0 +1,54 @@
+from math import *
+
+import pygame
+from image import *
+from constant import *
+
+
+class Bullet: 
+    def __init__(self, coords, spd):
+        """
+        Создание пули. Надо указать координаты стреляющего корабля, его скорость, путь до картинки с пулей, экран для рисования
+        Атрибуты:
+        __image - объект класса Image, создаваемый на основе BULLETIMG
+        __coords - координаты пули (в начале совпадают с координатами корабля)
+        __spd - скорость пули (высчитывается, как скорость относительно корабля + скорость корабля)
+        __heatrad - радиус, на котором пуля видна
+        __is_dead - уничтожена ли пуля
+        """
+        self.__image = Image(BULLETIMG[0])
+        self.__coords = coords
+        self.__spd = spd + spd/vec_len(spd)*BULLET_SPD
+        self.__heatrad = 0 #Об этом договоримся
+        self.__is_dead = False
+
+    def move(self, scale):
+        """Перемещение пули за TIME_PERIOD
+        При каждом вызове пуля сразу отрисовывается с помощь метода класса Image
+        """
+        self.__coords += self.__spd*TIME_PERIOD
+        self.__image.draw(0, self.__coords, scale)
+
+    def get_coord(self):
+        """Получить координаты пули """
+        return self.__coords
+
+    def get_heatrad(self):
+        """Получить 'радиус видимости' пули  """
+        return self.__heatrad
+
+    def get_dead(self):
+        """Узнать, уничтожена ли пуля """
+        return self.__is_dead
+
+    def set_dead(self, bool_var):
+        """Изменить статус пули """
+        self.__is_dead = bool_var
+
+    def collision_with_ship(self, ship):
+        """Столкновение пули с кораблем """
+        s_bul_ship = sqrt((self.__coords[0]-ship.get_coord()[0])**2 + (self.__coords[1]-ship.get_coord()[1])**2)
+        if s_bul_ship <= self.__heatrad + ship.get_hitrad():
+            ship.get_injured()
+            self.__is_dead = True
+        
